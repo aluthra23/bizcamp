5d4b6954dd2819454b44c5e476383cf353640fbb
Arav Luthra <aravluthra@gmail.com>
2025-04-12 04:00:53 -0400

    updated

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -2,10 +2,11 @@ import motor.motor_asyncio
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv("../.env")
 
 MONGO_URI = os.getenv("MONGODB_URI")
 DB_NAME = "biz_data"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
+db['users'].insert_one({'data':'hi'})
