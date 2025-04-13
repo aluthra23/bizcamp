cc0b941140d2cf9a20745abc3a0e3311967ba622
Narain Sriram <narainsriram@gmail.com>
2025-04-13 02:01:58 -0400

    ui edits

--- frontend/src/app/home/layout.tsx
+++ frontend/src/app/home/layout.tsx
@@ -39,15 +39,7 @@ export default function AppLayout({
                 </div>
 
                 <div className="flex items-center space-x-4">
-                    <button className="text-white/80 hover:text-white transition">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                            <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-                        </svg>
-                    </button>
-                    <div className="w-8 h-8 rounded-full bg-gradient-to-r from-primary to-accent flex items-center justify-center text-white font-medium">
-                        {user.initial}
-                    </div>
+                    {/* Removed notification bell and user avatar */}
                 </div>
             </header>
 
